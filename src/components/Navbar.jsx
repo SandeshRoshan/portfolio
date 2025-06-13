2780b92a9697fdbf94e3e6780e22b3414872d0ee
SandeshRoshan <srmilaptop@outlook.com>
2025-06-13 14:00:51 +0530

    13June24

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
 import { BsGithub, BsLinkedin, BsTwitterX, BsYoutube } from "react-icons/bs";
-import { BiX,BiMenu } from "react-icons/bi";
+import { BiX, BiMenu } from "react-icons/bi";
 
 const Navbar = () => {
 
   const [isOpen, setIsOpen] = useState(false);
 
-  const menuOpen = ()=>{
+  const menuOpen = () => {
     setIsOpen(!isOpen)
   }
 
@@ -26,63 +26,50 @@ const Navbar = () => {
         </a>
         <a href="#contact" className="cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100">
           <li>Contact</li>
-        </a> 
+        </a>
       </ul>
 
       <ul className="hidden md:flex gap-5">
-        <li className="cursor-pointer text-xl opacity-70 transition-all duration-300 hover:text-red-500 hover:opacity-100">
-          <a href="https://www.youtube.com/watch?v=k3JMkRwd_Nw" target="_blank"><BsYoutube/></a>
-          
-        </li>
         <li className="cursor-pointer text-xl opacity-70 transition-all duration-300 hover:text-blue-500 hover:opacity-100">
-          <a href="https://in.linkedin.com/in/sandesh-roshan-webdev?trk=people-guest_people_search-card" target="_blank"><BsLinkedin/></a>
+          <a href="https://in.linkedin.com/in/sandesh-roshan-webdev?trk=people-guest_people_search-card" target="_blank"><BsLinkedin /></a>
         </li>
-        {/* <li className="cursor-pointer text-xl opacity-70 transition-all duration-300 hover:text-sky-500 hover:opacity-100">
-          <BsTwitterX/>
-        </li> */}
         <li className="cursor-pointer text-xl opacity-70 transition-all duration-300 hover:text-orange-500 hover:opacity-100">
-          <a href="https://github.com/SandeshRoshan" target="_blank"><BsGithub/></a>
+          <a href="https://github.com/SandeshRoshan" target="_blank"><BsGithub /></a>
         </li>
       </ul>
 
-      {isOpen?(
-        <BiX className="block md:hidden text-4xl" onClick={menuOpen}/>
-      ):(
-        <BiMenu className="block md:hidden text-4xl" onClick={menuOpen}/>
+      {isOpen ? (
+        <BiX className="block md:hidden text-4xl" onClick={menuOpen} />
+      ) : (
+        <BiMenu className="block md:hidden text-4xl" onClick={menuOpen} />
       )}
 
 
       {isOpen && (
-        <div className={`fixed right-0 top-[84px] flex h-screen w-1/2 flex-col items-start justify-start gap-10 border-1 border-gray-800 bg-black/90 p-12 ${isOpen ? "block":"hidden"} `}>
+        <div className={`fixed right-0 top-[84px] flex h-screen w-1/2 flex-col items-start justify-start gap-10 border-1 border-gray-800 bg-black/90 p-12  ${isOpen ? "block" : "hidden"} `}>
           <ul className="flex flex-col gap-8">
             <a href="#home" className="cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100">
-          <li>Home</li>
-        </a>
-        <a href="#tech" className="cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100">
-          <li>Tech</li>
-        </a>
-        <a href="#projects" className="cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100">
-          <li>Projects</li>
-        </a>
-        <a href="#contact" className="cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100">
-          <li>Contact</li>
-        </a> 
+              <li>Home</li>
+            </a>
+            <a href="#tech" className="cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100">
+              <li>Tech</li>
+            </a>
+            <a href="#projects" className="cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100">
+              <li>Projects</li>
+            </a>
+            <a href="#contact" className="cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100">
+              <li>Contact</li>
+            </a>
           </ul>
 
           <ul className="flex flex-wrap gap-8">
-        <li className="cursor-pointer text-xl opacity-70 transition-all duration-300 hover:text-red-500 hover:opacity-100">
-          <BsYoutube/>
-        </li>
-        <li className="cursor-pointer text-xl opacity-70 transition-all duration-300 hover:text-blue-500 hover:opacity-100">
-          <BsLinkedin/>
-        </li>
-        <li className="cursor-pointer text-xl opacity-70 transition-all duration-300 hover:text-sky-500 hover:opacity-100">
-          <BsTwitterX/>
-        </li>
-        <li className="cursor-pointer text-xl opacity-70 transition-all duration-300 hover:text-orange-500 hover:opacity-100">
-          <BsGithub/>
-        </li>
-      </ul>
+            <li className="cursor-pointer text-xl opacity-70 transition-all duration-300 hover:text-blue-500 hover:opacity-100">
+              <a href="https://in.linkedin.com/in/sandesh-roshan-webdev?trk=people-guest_people_search-card" target="_blank"><BsLinkedin /></a>
+            </li>
+            <li className="cursor-pointer text-xl opacity-70 transition-all duration-300 hover:text-orange-500 hover:opacity-100">
+              <a href="https://github.com/SandeshRoshan" target="_blank"><BsGithub /></a>
+            </li>
+          </ul>
 
         </div>
       )}
